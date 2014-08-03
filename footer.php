ca45a84a789ccc32bf91b22e5b9f5aeedb2667a2
David Dinsdale <ddinsdale2000@yahoo.com>
2014-08-03 12:36:40 +0100

    Added direct link from the footer to GitHub

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     <footer id="footer" style="clear:both;text-align:center;  margin-top:2em; float:bottom;  ">
 
       <p>&nbsp;</p>
-      <p style = "border-top:solid blue 1px; font-size:small"> Provided free as an open source project - source code can be found by searching for govspend.org.uk on github.com</p>
+      <p style = "border-top:solid blue 1px; font-size:small"> Provided free as an open source project - why not join us developing the service? Source code can be found GitHub <a href = "https://github.com/ddinsdale2000/govspend.org.uk.git">GovSpend GitHub</a></p>
 
     </footer>

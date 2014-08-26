596aa1e6920a8a350217e0ea845808cd431d9c55
David Dinsdale <ddinsdale2000@yahoo.com>
2014-08-26 14:30:18 +0100

    Updated with August data

--- news.php
+++ news.php
@@ -22,55 +22,66 @@
 
 <h1>G-Cloud News</h1>
 
-<h2>16th April 2014 - G-Cloud Spend to end of March 2014</h2>
-<p>Spend for March 2014 has now been included.</p><p>Highlights are:</p> 
-<ul>
-<li>Very impressive growth - G-Cloud spend per month (£26m in March 2014) is now more than three times that of the entire year in 2012 (£7m)</li>
-<li>Spend in March was £26m.  March spend usually spikes - a trend figure would be about £19m.</li>
-<li>Total spend is £154m with almost £60m in 2014  (£87m being in 2013, £7.1m in 2012).</li>
-</ul>
-<p>Thanks to Cabinet Office for enhanced data</p>
-<ul>
-<li>Cabinet Office has helpfully enhanced the data this month, providing sector (of the customer) and whether the supplier is a SME or not.  
-We have incorporated both in the analysis now.  You will find an updated list of sectors and a breakdown of SME spend / non SME spend on the G-Cloud Spend page.</li>
-</ul>
 
-<p>Is government enhancing supplier diversity? It seems so.  The organisations that have engaged more than 10 suppliers via G-Cloud are:</p>
+<h2>26th August 2014 - G-Cloud Spend to end of July 2014</h2>
+<p>Spend for July 2014 has now been included.  Highlights are:</p> 
+
+
+
+<p><b>Overall spend stats</b></p> 
+
 <ul>
-<li>Cabinet Office - 57 suppliers</li>
-<li>Home Office - 56 suppliers</li>
-<li>MoJ - 37 suppliers</li>
-<li>Defra - 21 suppliers</li>
-<li>DoH - 14 suppliers</li>
-<li>DfE - 13 suppliers</li>
-<li>HMRC - 12 suppliers</li>
-<li>Maritime & Coastguard Agency - 12 suppliers</li>
-<li>BIS - 11 suppliers</li>
-<li>FCO - 10 suppliers</li>
+<li>Total spend is £249m with £154m so far in 2014  (£87m being in 2013, £7.1m in 2012).</li><br>
+<li>Spend with Small and Medium Sized Enterprises (SMEs) continues at pace.  We expect spend with SMEs for 2014 to be over £130m, compared to £42m in 2013.  SME spend to date in 2014 is £88m (57% of total spend).</li> <br>
+<li>Spend in July is £21m.  We expect this figure to be revised upwards to about £25m when the August figures are released. </li><br>   
 </ul>
 
 
-<p>Spread of the framework</p>
+<?php
+//<p>Is government enhancing supplier diversity? It seems so.  The organisations that have engaged more than 10 suppliers via G-Cloud are:</p>
+//<ul>
+//<li>Cabinet Office - 57 suppliers</li>
+//<li>Home Office - 56 suppliers</li>
+//<li>MoJ - 37 suppliers</li>
+//<li>Defra - 21 suppliers</li>
+//<li>DoH - 14 suppliers</li>
+//<li>DfE - 13 suppliers</li>
+//<li>HMRC - 12 suppliers</li>
+//<li>Maritime & Coastguard Agency - 12 suppliers</li>
+//<li>BIS - 11 suppliers</li>
+//<li>FCO - 10 suppliers</li>
+//</ul>
+?>
+
+
+<p><b>Spread of the framework</b></p>
 <ul>
-<li>23 new customers joined the framework this month (bringing the total to 437).</li> 
-<li>21 new suppliers joined the framework this month (bringing the total to 300).</li> 
+<li>The number of customers continues to grow strongly - up 27 in July (bringing the total to 551).</li> <br>
+<li>The number of suppliers is growing - up 23 in July (bringing the total to 366).</li> 
 </ul>
-<p>Forecasts</p>
+<p><b>Forecasts</b></p>
 <ul>
-<li>Spend is building strongly.  The framework is adding 20 to 30 new buying organisations each month.</li>
-<li>It is likely that that spend will regularly top £20m a month this quarter and be around £30m a month by year end.</li>
-<li>We are holding our forecast for 2014 to £200m but if growth continues, will be revising this upwards in coming months.  £250m is looking achievable and £300m is within sight.  </li>
+<li>We are holding our forecast for 2014 at £250m, however, this figure is looking under pressure - £280m is possible if growth continues. </li><br>
+<li>The trend figure for spend topped £20m a month for the first time in April and is looking robust at that level.  We expect average monthly spend to increase by about £1m a month to £30m a month by year end.</li><br>
 </ul>
+
+
+
 <h3>You are welcome to use and copy any information on this site (including the presentations) as long as you attribute the source as GovSpend.Org.UK </h3>
-<p>
+<p>We are currently updating the presentation.  Will be there tonight.</p>
+<?php
 
-<iframe src="http://www.slideshare.net/slideshow/embed_code/33615860" width="476" height="400" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
-</p>
-
-
-<h3>Previous news releases and presentations can be found at</h3>
-<p> <a href="news-2014-02.php">G-Cloud News - Feb 2014</a></p>
-<p> <a href="news-2014-01.php">G-Cloud News - Jan 2014</a></p>
+//<p>
+//<iframe src="//www.slideshare.net/slideshow/embed_code/37509219" width="476" height="400" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+//</p>
+?>
+<h3>Previous news releases and presentations can be found at:</h3>
+<p> <a href="news-2014-06.php">G-Cloud News - Aug 2014</a></p>
+<p> <a href="news-2014-05.php">G-Cloud News - Jun 2014</a></p>
+<p> <a href="news-2014-04.php">G-Cloud News - May 2014</a></p>
+<p> <a href="news-2014-03.php">G-Cloud News - Apr 2014</a></p>
+<p> <a href="news-2014-02.php">G-Cloud News - Mar 2014</a></p>
+<p> <a href="news-2014-01.php">G-Cloud News - Feb 2014</a></p>
 <p> <a href="news-2013-12.php">G-Cloud News - Dec 2013</a></p>
 <p> <a href="news-2013-11.php">G-Cloud News - Nov 2013</a></p>
 <p> <a href="news-2013-10.php">G-Cloud News - Oct 2013</a></p>

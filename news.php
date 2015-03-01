b6365a94dfede006a3da3d63673b5482f00becdb
David Dinsdale <ddinsdale2000@yahoo.com>
2015-03-01 18:41:47 +0000

    Updated news

---
+++ news.php
@@ -0,0 +1,108 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>GovSpend.org.uk - News - helping you find more business in Government</title>
+  <link rel="stylesheet" href="main.css" type="text/css" />
+      <link rel="stylesheet" href="table.css" type="text/css" />
+
+</head>
+<body>
+<?php 
+/* Page Name:  	Contact Us
+ * Desc:      	
+ *
+ */
+    include_once("analyticstracking.php");
+    include('header_main.php'); 
+    include('menu1.php');
+    include('init.php');
+?> 
+
+<h1>G-Cloud News</h1>
+
+
+<h2>In summary</h2>
+
+<p>28th Feb 2015</p> 
+
+<ul>
+<li>Total spend is via G-Cloud is £467m</li><br>
+<li>Spend by year is £25m in 2015 (so far), £347m in 2014, £87m in 2013 and £7.1m in 2012.</li><br>
+<li>Spend with Small and Medium Sized Enterprises (SMEs) continues at pace - £13m in 2015 so far, £171m in 2014 with £42m in 2013. </li> <br>
+<li>Spend peaked in November 2014 at £43m.  Monthly spend in 2014 increased by an average of £1.8m a month from £16m in January to £41m in December.</li>
+<li>Expect the January figure to be revised upwards to over £40m when the February 2015 data is released.</li>
+</ul>
+
+
+<?php
+//<p>Is government enhancing supplier diversity? It seems so.  The organisations that have engaged more than 10 suppliers via G-Cloud are:</p>
+//<ul>
+//<li>Cabinet Office - 57 suppliers</li>
+//<li>Home Office - 56 suppliers</li>
+//<li>MoJ - 37 suppliers</li>
+//<li>Defra - 21 suppliers</li>
+//<li>DoH - 14 suppliers</li>
+//<li>DfE - 13 suppliers</li>
+//<li>HMRC - 12 suppliers</li>
+//<li>Maritime & Coastguard Agency - 12 suppliers</li>
+//<li>BIS - 11 suppliers</li>
+//<li>FCO - 10 suppliers</li>
+//</ul>
+
+?>
+
+
+<p><b>2015 forecasts</b></p>
+<p>Our forecast for G-Cloud spend in 2015 is between £500m and £600m</p>
+<p>There are three main factors that will influence total G-Cloud spend, two positively and one negatively:</p>
+<ul>
+<li>The number of buyers continues at pace, rising from 346 in Dec 2013 to 695 in Dec 2014.  
+As new organisations continue to join the framework, the total spend will increase and could lead to a significant overshoot 
+of our 2015 forecast.  Spend of £750m in 2015 would not be a surprise. We are analysing the 2014 data to see how much 'new' buyers contributed in 2014.  We will publish the data once we have it but a swift review indicates that we expect new buyers to contribute an additional £200m in 2015.</li><br>
+<li>More large IT contracts expire in 2015.  A quick review of the spend data suggests that once a buyer starts using G-Cloud, they expand the number of purchases quickly.  We expect that this behaviour will add about £100m to total spend in 2015.  
+Again, we are analysing the data and will publish our finding when we have them.</li><br>
+<li>Inline with Government strategy, we expect that some G-Cloud spend will move to other frameworks, particularly the Digital Services Framework for Agile developers.  We forecast that about £100m will move from G-Cloud to Digital Services and other frameworks.</li>
+</ul>
+<p>The net effect of these three factors will be an increase in G-Cloud spend of about £200m.
+
+<p><b>Review of Jan 2015 data</b></p>
+<ul>
+<li>The number of customers continues to grow strongly - up 21 in January (bringing the total to 716).</li> <br>
+<li>The number of suppliers is growing - up 21 in January (bringing the total to 476).</li> 
+</ul>
+<ul>
+<li>The trend figure for spend has increased and is now about $40m a month.  Average monthly spend is increasing by about £1.8m a month. </li><br>
+</ul>
+
+<h3>You are welcome to use and copy any information on this site (including the presentations) as long as you attribute the source as GovSpend.Org.UK </h3>
+
+<p>
+<iframe src="//www.slideshare.net/slideshow/embed_code/45279109" width="476" height="400" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>
+<?php
+//We are working on the usual presentation.
+?>
+</p>
+
+<h3>Previous news releases and presentations can be found at:</h3>
+<p> <a href="news-2014-12.php">G-Cloud News - Jan 2014</a></p>
+<p> <a href="news-2014-11.php">G-Cloud News - Dec 2014</a></p>
+<p> <a href="news-2014-10.php">G-Cloud News - Nov 2014</a></p>
+<p> <a href="news-2014-09.php">G-Cloud News - Oct 2014</a></p>
+<p> <a href="news-2014-08.php">G-Cloud News - Sep 2014</a></p>
+<p> <a href="news-2014-07.php">G-Cloud News - Aug 2014</a></p>
+<p> <a href="news-2014-06.php">G-Cloud News - Jul 2014</a></p>
+<p> <a href="news-2014-05.php">G-Cloud News - Jun 2014</a></p>
+<p> <a href="news-2014-04.php">G-Cloud News - May 2014</a></p>
+<p> <a href="news-2014-03.php">G-Cloud News - Apr 2014</a></p>
+<p> <a href="news-2014-02.php">G-Cloud News - Mar 2014</a></p>
+<p> <a href="news-2014-01.php">G-Cloud News - Feb 2014</a></p>
+<p> <a href="news-2013-12.php">G-Cloud News - Dec 2013</a></p>
+<p> <a href="news-2013-11.php">G-Cloud News - Nov 2013</a></p>
+<p> <a href="news-2013-10.php">G-Cloud News - Oct 2013</a></p>
+
+
+<?php include ("footer.php"); ?>
+</body>
+</html>

b447eadb42314f9977bd36c0ae61fcc3bea9328e
David Dinsdale <ddinsdale2000@yahoo.com>
2015-07-15 22:06:02 +0100

    Update to news

---
+++ news.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>GovSpend.org.uk - News - helping you find more business in Government</title>
+  <link rel="stylesheet" href="main.css" type="text/css" />
+      <link rel="stylesheet" href="table.css" type="text/css" />
+
+</head>
+<body>
+<?php 
+/* Page Name: News
+ * Desc: Has the latest news for the data in govspend.org.uk     	
+ *
+ */
+    include_once("analyticstracking.php");
+    include('header_main.php'); 
+    include('menu1.php');
+    include('init.php');
+?> 
+
+<h1>G-Cloud News</h1>
+
+
+<h2>In summary</h2>
+
+<p><b>Review of May data</b></p>
+<ul>
+<li>The number of customers continues to grow - up 15  (bringing the total to 816).</li> <br>
+<li>The number of suppliers is growing - up 15 (bringing the total to 566).</li> <br>
+
+</ul>
+
+<p><b>Overall framework stats</b></p> 
+
+<ul>
+<li>Total spend is via G-Cloud is £639m</li><br>
+<li>Spend by year is £191m in 2015 (so far), £353m in 2014, £87m in 2013 and £6.6m in 2012.</li><br>
+<li>Spend with Small and Medium Sized Enterprises (SMEs) continues at pace - £97m in 2015 so far, £173m in 2014 with £42m in 2013. </li> <br>
+<li>After a what looks like a pre-election boost, monthly spend is stabilising around £40m a month (expect May figures to be revised upwards by about £10m when the next data set is released).</li>
+</ul>
+
+<p><b>Changes this month</b></p> 
+
+<ul>
+<li>The only frustrating part of running this service is that Cabinet Office tends to change the format of the data feed without notice.  This month the 
+data has a new list of sectors.  We have asked whether this is an intended change.</li><br>
+</ul>
+
+
+
+<h3>Previous news releases and presentations can be found at:</h3>
+<p> <a href="news-2015-04.php">G-Cloud News - May 2015</a></p>
+<p> <a href="news-2015-03.php">G-Cloud News - Apr 2015</a></p>
+<p> <a href="news-2015-02.php">G-Cloud News - Mar 2015</a></p>
+<p> <a href="news-2015-01.php">G-Cloud News - Feb 2015</a></p>
+<p> <a href="news-2014-12.php">G-Cloud News - Jan 2015</a></p>
+<p> <a href="news-2014-11.php">G-Cloud News - Dec 2014</a></p>
+<p> <a href="news-2014-10.php">G-Cloud News - Nov 2014</a></p>
+<p> <a href="news-2014-09.php">G-Cloud News - Oct 2014</a></p>
+<p> <a href="news-2014-08.php">G-Cloud News - Sep 2014</a></p>
+<p> <a href="news-2014-07.php">G-Cloud News - Aug 2014</a></p>
+<p> <a href="news-2014-06.php">G-Cloud News - Jul 2014</a></p>
+<p> <a href="news-2014-05.php">G-Cloud News - Jun 2014</a></p>
+<p> <a href="news-2014-04.php">G-Cloud News - May 2014</a></p>
+<p> <a href="news-2014-03.php">G-Cloud News - Apr 2014</a></p>
+<p> <a href="news-2014-02.php">G-Cloud News - Mar 2014</a></p>
+<p> <a href="news-2014-01.php">G-Cloud News - Feb 2014</a></p>
+<p> <a href="news-2013-12.php">G-Cloud News - Dec 2013</a></p>
+<p> <a href="news-2013-11.php">G-Cloud News - Nov 2013</a></p>
+<p> <a href="news-2013-10.php">G-Cloud News - Oct 2013</a></p>
+
+
+<?php include ("footer.php"); ?>
+</body>
+</html>
